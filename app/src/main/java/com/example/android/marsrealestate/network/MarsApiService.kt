f1e61925d81eb35fe777dc9ec0cbffdb4b15a9aa
Kevin Beaton <kabton14@gmail.com>
2021-12-05 22:12:36 -0500

    Add moshi object to retrofit builder

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -31,7 +31,7 @@ private val moshi = Moshi.Builder()
     .build()
 
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
