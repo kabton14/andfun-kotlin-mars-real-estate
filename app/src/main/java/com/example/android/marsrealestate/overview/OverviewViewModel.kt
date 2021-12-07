c0e3d32f01f44713b296da1742d9cd26ef587283
Kevin Beaton <kabton14@gmail.com>
2021-12-07 04:10:29 -0500

    Rename response LiveData to status

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -34,11 +34,11 @@ import retrofit2.Response
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<String>()
 
     // The external immutable LiveData for the request status String
-    val response: LiveData<String>
-        get() = _response
+    val status: LiveData<String>
+        get() = _status
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
