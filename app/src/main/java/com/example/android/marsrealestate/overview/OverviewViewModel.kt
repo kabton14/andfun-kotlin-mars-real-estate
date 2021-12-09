0050d68a67c0a6faa5b6c168412723260515c0bc
Kevin Beaton <kabton14@gmail.com>
2021-12-09 13:14:26 -0500

    Change _status type from String to MarsApiStatus

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -36,10 +36,10 @@ class OverviewViewModel : ViewModel() {
     enum class MarsApiStatus { LOADING, ERROR, DONE }
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the request status String
-    val status: LiveData<String>
+    val status: LiveData<MarsApiStatus>
         get() = _status
 
     private val _properties = MutableLiveData<List<MarsProperty>>()
