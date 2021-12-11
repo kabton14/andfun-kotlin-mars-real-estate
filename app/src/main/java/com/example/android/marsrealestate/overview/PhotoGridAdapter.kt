90397bbc245f0cc28805230d0c9c35517de03251
Kevin Beaton <kabton14@gmail.com>
2021-12-10 23:51:47 -0500

    Add an OnClickListener parameter to the PhotoGridAdapter class declaration

--- app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
@@ -25,7 +25,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.android.marsrealestate.databinding.GridViewItemBinding
 import com.example.android.marsrealestate.network.MarsProperty
 
-class PhotoGridAdapter : ListAdapter<MarsProperty, PhotoGridAdapter.MarsPropertyViewHolder>(DiffCallback){
+class PhotoGridAdapter(private val onClickListener: OnClickListener  ) : ListAdapter<MarsProperty, PhotoGridAdapter.MarsPropertyViewHolder>(DiffCallback){
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
