4e94a24127d540382347ea7dd2b365ef4dd897d6
Kevin Beaton <kabton14@gmail.com>
2021-12-12 22:42:57 -0500

    Check if property is a rental

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -27,4 +27,7 @@ data class MarsProperty(
     @Json(name = "img_src")
     val imgSrcUrl: String,
     val type: String,
-    val price: Double ) : Parcelable
+    val price: Double ) : Parcelable {
+        val isRental
+            get() = type == "rent"
+    }
